378f708de6568a9c00466675fae064f70c87f0a5
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-07-01 01:21:17 +0200

    Automatic Build Commit

--- src/commands/layout/CreateLayoutCommand.ts
+++ src/commands/layout/CreateLayoutCommand.ts
@@ -22,7 +22,6 @@ import { ProjectLayoutUpdater } from '../../updaters/ProjectLayoutUpdater';
 import { ReadmeUpdater } from '../../updaters/ReadmeUpdater';
 import { AppVersionUpdater } from '../../updaters/AppVersionUpdater';
 import { DockerfileUpdater } from '../../updaters/DockerfileUpdater';
-import { BuildfileUpdater } from '../../updaters/BuildfileUpdater';
 import { DevContainerUpdater } from '../../updaters/DevContainerUpdater';
 import { UnitTestsUpdater } from '../../updaters/UnitTestsUpdater';
 import { Log } from '../../helpers/Log';
@@ -61,7 +60,6 @@ export class CreateLayoutCommand extends Command<LayoutCommandOptions> {
             await mgr.update(new AppVersionUpdater());
             await mgr.update(new ReadmeUpdater());
             await mgr.update(new DockerfileUpdater());
-            await mgr.update(new BuildfileUpdater());
             await mgr.update(new DevContainerUpdater());
             await mgr.update(new UnitTestsUpdater());
 
